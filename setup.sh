a378b204335f4c67ce1ac4fde751e36a5e4868bd
Karst Hammer <karst@karsthammer.nl>
2013-03-11 00:23:43 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Setting up your vim settings"
+cd $(dirname "$0")
+
+echo "Installing Vundler"
+git submodule init
+git submodule update
+
+echo "Installing bundles"
+vim -s .bundle_install 2>/dev/null
+
+echo "Finished setting up vim"
+
+cd -
